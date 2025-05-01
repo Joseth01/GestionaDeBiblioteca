04e91e4c85ddc0827d8c3832a671430a785959dd
JOSETH.ROJAS@ucr.ac.cr <JOSETH.ROJAS@ucr.ac.cr>
2025-05-01 01:31:37 -0600

    M

--- AplicacionqueGestionaUnaBiblioteca.BL/GestorDeLaBiblioteca.cs
+++ AplicacionqueGestionaUnaBiblioteca.BL/GestorDeLaBiblioteca.cs
@@ -8,5 +8,6 @@ namespace AplicacionqueGestionaUnaBiblioteca.BL
 {
     class GestorDeLaBiblioteca
     {
+
     }
 }
--- AplicacionqueGestionaUnaBiblioteca.Model/Libro.cs
+++ AplicacionqueGestionaUnaBiblioteca.Model/Libro.cs
@@ -12,6 +12,7 @@
 
         public string elTipo { get; set; } 
 
-        public string elEstado { get; set; } 
+        public string elEstado { get; set; }
+        
     }
 }
